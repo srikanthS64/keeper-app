ba8c4edd8d8b503c55119ab834583cbf6ef355c7
srikanth S <103022860+srikanthS64@users.noreply.github.com>
2023-01-21 11:47:39 +0530

    Add files via upload

---
+++ src/notes.js
@@ -0,0 +1,18 @@
+const notes = [
+    {
+     id: 1,
+    title: "A",
+    content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    },
+    {
+        id: 2,
+       title: "B",
+       content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    },
+    {
+        id: 3,
+       title: "C",
+       content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+    },
+]
+export default notes;
